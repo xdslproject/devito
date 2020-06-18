1783347b1ad2592fad314fe01fce5da7a85268e7
George BIsbas <g.bisbas18@imperial.ac.uk>
2020-06-18 21:04:28 +0300

    saveup

--- tests/test_dse.py
+++ tests/test_dse.py
@@ -228,6 +228,28 @@ def test_makeit_ssa(exprs, exp_u, exp_v):
     assert np.all(v.data == exp_v)
 
 
+def test_implicit_invariant():
+
+    x = Dimension(name='x')
+    y = Dimension(name='y')
+    time = TimeDimension(name='time')
+
+    g = TimeFunction(name='g', shape=(1, 3), dimensions=(time, x),
+                        time_order=0, dtype=np.int32)
+    g.data[0, :] = [1, 2, 3]
+    h1 = TimeFunction(name='h1', shape=(1, 1), dimensions=(time, y), time_order=0)
+    h1.data[0, 0] = 0
+
+    eq0 = Eq(y.symbolic_max, g[0, x], implicit_dims=(time, x))
+    eq1 = Eq(h1[0, 0], 1, implicit_dims=(time, x, y))
+
+    op = Operator([eq0, eq1])
+    print(op.ccode)
+    op.apply()
+
+    assert(h1.data == 9.)
+
+
 @pytest.mark.parametrize('opt', ['noop', 'advanced'])
 def test_time_dependent_split(opt):
     grid = Grid(shape=(10, 10))
