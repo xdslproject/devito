bb634dc6fc7d53c6a54576f82cb7855d6b96c941
George Bisbas <georgeb@bud32.dugeo.com>
2020-12-19 01:38:10 +0800

    upd

--- sweep.sh
+++ sweep.sh
@@ -1,21 +1,21 @@
 # adapt the following line according your machine
 
 # setup
-NX=176
-NY=176
-NZ=176
-ORDER=8
+NX=688
+NY=688
+NZ=688
+ORDER=24
 
 # tests all tile shapes where BLOCK_X*BLOCK_Y*BLOCK_Z <= 1024 and multiple of 32
-for BLOCK_X in 2 4 8 16 32 64; do
-    for BLOCK_Y in 2 4 8 16 32 64; do
-        for BLOCK_Z in 2 4 8 16 32 64; do
+for BLOCK_X in 4 8 16 32 64; do
+    for BLOCK_Y in 4 8 16 32 64; do
+        for BLOCK_Z in 4 8 16 32 64; do
             PRODUCT=$(($BLOCK_X*$BLOCK_Y*$BLOCK_Z))
             if [ $PRODUCT -le 1024 ] && [ $(($PRODUCT%32)) -eq 0 ]
             then
                 echo "==========" $BLOCK_X $BLOCK_Y $BLOCK_Z
                 export TILE_SIZE=$BLOCK_X,$BLOCK_Y,$BLOCK_Z
-                DEVITO_PLATFORM=nvidiaX DEVITO_LANGUAGE=openacc DEVITO_ARCH=pgcc DEVITO_LOGGING=DEBUG python examples/seismic/acoustic/acoustic_example.py -so $ORDER -d $NX $NY $NZ
+                DEVITO_PLATFORM=nvidiaX DEVITO_LANGUAGE=openacc DEVITO_ARCH=pgcc DEVITO_LOGGING=DEBUG python examples/seismic/acoustic/acoustic_example.py -so $ORDER -d $NX $NY $NZ --tn 688
             fi
         done
     done
