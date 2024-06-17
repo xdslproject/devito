69ef2609b7d1fd4a77e6773516c326af9f42b5a2
George Bisbas <g.bisbas18@imperial.ac.uk>
2024-06-17 19:13:48 +0100

    compiler: Use Insert Point for Deprecated insert_op_xxx

--- devito/ir/ietxdsl/cluster_to_ssa.py
+++ devito/ir/ietxdsl/cluster_to_ssa.py
@@ -28,6 +28,7 @@ from xdsl.pattern_rewriter import (
     PatternRewriteWalker,
     RewritePattern,
     op_type_rewrite_pattern,
+    InsertPoint
 )
 from xdsl.builder import ImplicitBuilder
 from xdsl.transforms.experimental.convert_stencil_to_ll_mlir import StencilToMemRefType
@@ -757,6 +758,49 @@ class WrapFunctionWithTransfers(GPURewritePattern):
                 dealloc = gpu.DeallocOp(alloc)
                 body.insert_ops_before([copy_out, dealloc], body.ops.last)
         rewriter.insert_op_after_matched_op(wrapper)
+
+
+class TimerRewritePattern(RewritePattern):
+    """
+    Base class for time benchmarking related rewrite patterns
+    """
+    pass
+
+
+@dataclass
+class MakeFunctionTimed(TimerRewritePattern):
+    """
+    Populate the section0 devito timer with the total runtime of the function
+    """
+    func_name: str
+    seen_ops: set[func.Func] = field(default_factory=set)
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: func.FuncOp, rewriter: PatternRewriter):
+        if op.sym_name.data != self.func_name or op in self.seen_ops:
+            return
+
+        # only apply once
+        self.seen_ops.add(op)
+
+        # Insert timer start and end calls
+        rewriter.insert_op([
+            t0 := func.Call('timer_start', [], [builtin.f64])
+        ], InsertPoint.at_start(op.body.block))
+
+        ret = op.get_return_op()
+        assert ret is not None
+
+        rewriter.insert_op_before([
+            timers := iet_ssa.LoadSymbolic.get('timers', llvm.LLVMPointerType.opaque()),
+            t1 := func.Call('timer_end', [t0], [builtin.f64]),
+            llvm.StoreOp(t1, timers),
+        ], ret)
+
+        rewriter.insert_op([
+            func.FuncOp.external('timer_start', [], [builtin.f64]),
+            func.FuncOp.external('timer_end', [builtin.f64], [builtin.f64]),
+        ], InsertPoint.after(rewriter.current_operation))
 
 
 def get_containing_func(op: Operation) -> func.FuncOp | None:
